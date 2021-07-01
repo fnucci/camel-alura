8019ef836b6e0c25cfe1eafcee8cccdc5016e86c
Felipe Nucci <flpnucci@gmail.com>
2021-07-01 12:15:21 -0300

    Primeira rota com o apache camel

--- src/main/java/br/com/caelum/camel/RotaPedidos.java
+++ src/main/java/br/com/caelum/camel/RotaPedidos.java
@@ -1,6 +1,7 @@
 package br.com.caelum.camel;
 
 import org.apache.camel.CamelContext;
+import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.impl.DefaultCamelContext;
 
 public class RotaPedidos {
@@ -9,5 +10,21 @@ public class RotaPedidos {
 
 		CamelContext context = new DefaultCamelContext();
 
-	}	
+		context.addRoutes(new RouteBuilder() {
+
+			@Override
+			public void configure() throws Exception {
+				// TODO Auto-generated method stub
+				from("file:pedidos?delay=5s&noop=true")
+				.log("${exchange.pattern}")
+				.log("Camel trabalhando !!! - ${id} - ${body}")
+				.to("file:saidas");
+			}
+
+		});
+		
+		context.start();
+		Thread.sleep(20000);
+		context.stop();
+	}
 }
